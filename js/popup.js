8ea8281f97353615df2d3c541f2847831f46be24
Ben Lewis <benji@benjilewis.dev>
2024-02-19 19:08:43 +0000

    MVP Functionality
    The script works with harcoded values. TODO: config page & timeout

---
+++ js/popup.js
@@ -0,0 +1,68 @@
+// Function to display a popup
+function displayPopup() {
+	console.log('Popup displayed');
+	// Create an iframe element for the popup
+	const popupIframe = document.createElement('iframe');
+	popupIframe.id = 'columnCreationPopup';
+	popupIframe.style.position = 'fixed';
+	popupIframe.style.top = '50%';
+	popupIframe.style.left = '50%';
+	popupIframe.style.transform = 'translate(-50%, -50%)';
+	popupIframe.style.background = 'white';
+	popupIframe.style.padding = '20px';
+	popupIframe.style.border = '2px solid black';
+	popupIframe.style.zIndex = '99999999999999';
+	popupIframe.style.width = '500px'; // Set a width
+	popupIframe.style.height = '300px'; // Set a height
+
+	// Append the iframe to the body
+	document.body.appendChild(popupIframe);
+
+	// Add content to the popup
+	popupIframe.contentWindow.document.open();
+	popupIframe.contentWindow.document.write(`
+		<link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+		<style>
+			body {
+				font-family: 'Roboto', sans-serif;
+			}
+		</style>
+		<h2>Creating a new column?</h2>
+		<p>Check schema name is <strong>lowercase</strong>.</p>
+		<p>Is this a choice field? <strong>Append code</strong>.</p>
+		<p>Is this a lookup field? <strong>Append id</strong>.</p>
+		<button id="closePopupBtn">Close</button>
+	`);
+	popupIframe.contentWindow.document.close();
+
+	// Add event listener to close the popup when the close button is clicked
+	const closePopupBtn = popupIframe.contentWindow.document.getElementById('closePopupBtn');
+	closePopupBtn.addEventListener('click', () => {
+		popupIframe.remove();
+	});
+}
+
+// Function to observe changes in the DOM
+function observeDOM() {
+	const observer = new MutationObserver(iterateMutations);
+
+	observer.observe(document.body, { childList: true, subtree: true });
+}
+
+function iterateMutations(mutations) {
+	mutations.forEach((mutation) => {mutation.addedNodes.forEach(checkNode);});
+}
+
+function checkNode(node) {
+	// Check if the node is an element node
+	if (node.nodeType === Node.ELEMENT_NODE) {
+		const link = node.querySelector('a[href="https://go.microsoft.com/fwlink/?linkid=2192539"]');
+		// Check for the specific text
+		if ((node.textContent.includes('Previously called fields.') || (link && link.textContent === 'Learn More')) && !document.getElementById('columnCreationPopup')) {
+			displayPopup();
+		}
+	}
+}
+
+// Start observing DOM mutations
+observeDOM();
